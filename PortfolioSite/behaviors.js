8cfc2039857b5eaa5c2aca529b3f1c2a5912f854
Meleeman01 <meleeman01@live.com>
2018-05-28 18:05:13 -0500

    building Layout
    imported slick into my portfolio website, built nav and got slick to work with a local jquery to back up the CDN, as well as local slick stylesheets, some which are being overridden by my stylesheets. added some content. more to come before launch. hopefully there will be not many issues for mobile users.

---
+++ PortfolioSite/behaviors.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.page').slick({
+    
+  });
+});
+//below is for the footer
+var d=new Date();
+var n=d.getFullYear();
+document.getElementById("date").innerHTML=n;
