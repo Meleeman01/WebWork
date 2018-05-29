9659d8e59e6502ac310b167adefe26fba888ff0d
Meleeman01 <meleeman01@live.com>
2018-05-29 06:49:13 -0500

    built page 1 and moved the button control dealy thingy
    i probably should've checked to see if slick.js would've made the pages responsive, but then I was like "nah I wanna used grid and make it fit myself" so I spent like 8 hours doing that and then trying to figure out which classes control what, and what the code does. i'm not entirely sure but i'm going to get the menu to correspond with the buttons.

--- PortfolioSite/behaviors.js
+++ PortfolioSite/behaviors.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
   $('.page').slick({
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  dots:true,
+  appendDots:".navigation"
+
     
   });
-});
-//below is for the footer
+ $('#HomeN').click(function(){
+ 	alert("home was clicked");
+ });
+
+  //below is for the footer
 var d=new Date();
 var n=d.getFullYear();
 document.getElementById("date").innerHTML=n;
+});
